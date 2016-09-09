39321b67f184e4c73ca1e8742f5afb59b45de2c7
Shuyang Sun <sunbuffett@gmail.com>
2016-09-08 21:17:14 -0400

    Changed cost_func_test to use doctest.

--- cost_func_test.py
+++ cost_func_test.py
@@ -1,24 +1,37 @@
-import nn_alg as alg
+import doctest
 import scipy.io
-import util
 import numpy as np
+import nn_alg as alg
+import util
 
 
-mat_data = scipy.io.loadmat('digits_data/hand_written_digits.mat')
-X = mat_data['X']
-y = mat_data['y']
-augmented_matrix = np.append(X, y, axis=1)
-np.random.shuffle(augmented_matrix)
-X = augmented_matrix[:, :-1]
-y = augmented_matrix[:, -1]
-_, y = util.DataProcessor.get_unique_categories_and_binary_outputs(y)
+def cost_of_data():
+    """
+    Get the cost of given data and weights.
+    :return: The cost of given data.
+    >>> '{0:.6f}'.format(cost_of_data())
+    '0.287629'
+    """
+    mat_data = scipy.io.loadmat('digits_data/hand_written_digits.mat')
+    X = mat_data['X']
+    y = mat_data['y']
+    augmented_matrix = np.append(X, y, axis=1)
+    np.random.shuffle(augmented_matrix)
+    X = augmented_matrix[:, :-1]
+    y = augmented_matrix[:, -1]
+    _, y = util.DataProcessor.get_unique_categories_and_binary_outputs(y)
 
-mat_weights = scipy.io.loadmat('cost_test_data/ex4weights.mat')
-Theta1 = mat_weights['Theta1']
-Theta2 = mat_weights['Theta2']
+    mat_weights = scipy.io.loadmat('cost_test_data/ex4weights.mat')
+    Theta1 = mat_weights['Theta1']
+    Theta2 = mat_weights['Theta2']
 
-Theta = [Theta1, Theta2]
-h_theta_x = alg.nn_forward_prop(X, Theta)[-1]
+    Theta = [Theta1, Theta2]
+    h_theta_x = alg.nn_forward_prop(X, Theta)[-1]
 
-cost = alg.nn_J_Theta(h_theta_x, y, lamb=0, Theta=Theta)
-print(cost)
+    cost = alg.nn_J_Theta(h_theta_x, y, lamb=0, Theta=Theta)
+    return cost
+
+
+if __name__ == '__main__':
+    doctest.testmod()
+
